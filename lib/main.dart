a0cef7e7dcd8c2cf9be09bc5d9e2faa7045e20b9
MuhammadRahman2 <muhammadrahman2042@gmail.com>
2024-03-14 20:24:37 +0500

    home

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_learning/contact.dart';
-import 'package:flutter_learning/hive/hive_view.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-import 'package:path_provider/path_provider.dart' as path_provider;
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final appDocumentDir = await path_provider.getApplicationDocumentsDirectory();
-  Hive.init(appDocumentDir.path);
-  Hive.registerAdapter(ContactAdapter());
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -29,27 +16,7 @@ class _MyAppState extends State<MyApp> {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: FutureBuilder(
-          future: Hive.openBox('contacts'),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              if (snapshot.hasError) {
-                return Text(snapshot.error.toString());
-              } else {
-                return HiveView();
-              }
-            } else {
-              return const Center(
-                child: LinearProgressIndicator(),
-              );
-            }
-          }),
+      home: const Scaffold(body: Center(child: Text('Home'),),)
     );
-  }
-
-  @override
-  void dispose() {
-    Hive.close();
-    super.dispose();
   }
 }
