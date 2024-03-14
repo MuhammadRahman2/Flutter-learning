9213e8a1e108d0846fcdc633abcb53c2bf3868ee
MuhammadRahman2 <muhammadrahman2042@gmail.com>
2024-03-14 20:31:00 +0500

    zero point

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,11 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Scaffold(body: Center(child: Text('Home'),),)
+      home: const Scaffold(
+        body: Center(
+        child: Text('Home'),
+      ),
+      ),
     );
   }
 }
